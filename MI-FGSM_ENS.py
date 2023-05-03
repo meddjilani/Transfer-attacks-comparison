5aa1117a591ef1c84e95bac0481afc67429ef16a
meddjilani <km_djilani@esi.dz>
2023-05-03 21:47:25 +0200

    args ensemble

--- MI-FGSM_ENS.py
+++ MI-FGSM_ENS.py
@@ -9,89 +9,44 @@ import torchattacks_ens.attacks.mifgsm_ens as taamifgsm_ens
 import json
 import os
 from Normalize import Normalize
+import argparse
 
 #add:
     # args parm of mi
 if __name__ == '__main__':
-    
-    with open('config_ids_source_targets.json','r') as f:
-        config = json.load(f)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, nargs = '+',default=['Gowal2021Improving_28_10_ddpm_100m', 'Standard'])
+    parser.add_argument('--target', type=str, default= 'Carmon2019Unlabeled')
+    parser.add_argument('--n_examples', type=int, default=10)
+    parser.add_argument('--eps', type = float, default=8/255)
+    parser.add_argument('--alpha', type=float,default=2/255)
+    parser.add_argument('--decay', type=float,default= 1.0)
+    parser.add_argument('--steps', type=int,default=10)
+    args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    mean, std  = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
     source_models = []
-    for source in config['sources']:
-        if source == 'Resnet50': #use it
-            source_model = resnet50(pretrained=True)
-        elif source == 'Resnet34': 
-            source_model = resnet34(pretrained=True)
-        elif source  == 'Resnet18':
-            source_model = resnet18(pretrained=True)
-        elif source == 'Densenet169': 
-            source_model = densenet169(pretrained=True)
-        elif source == 'Densenet161': 
-            source_model = densenet161(pretrained=True)
-        elif source == 'Densenet121': 
-            source_model = densenet121(pretrained=True)
-        elif source == 'Vgg19':
-            source_model = vgg19_bn(pretrained=True)
-        elif source  == 'Resnet50_non_normalize': 
-            source_model = resnet50(pretrained=False)
-            PATH = 'resnet50_128_100'
-            source_model.load_state_dict(torch.load(PATH,map_location=device))
-        else :
-            raise ValueError('Source model is not recognizable')
-        
-        source_model = nn.Sequential(
-            Normalize(mean, std),
-            source_model
-        )
-        #todevice
-        source_model.eval()
+    for model_name in args.model:
+        source_model = load_model(model_name=model_name, dataset='cifar10', threat_model='Linf')
+        source_model.to(device)
         source_models.append(source_model)
 
-    print('sources',config['sources'], len(source_models))
-    target_models = [] 
-    rank_target_models = []
-    for rank, model_id in config['targets'].items():
-        print('Loading model ', rank)
-        target_model = load_model(model_name=model_id, dataset='cifar10', threat_model='Linf')
-        #todevice
-        target_models.append(target_model)
-        rank_target_models.append(rank)
+
+    target_model = load_model(model_name= args.target, dataset='cifar10', threat_model='Linf')
+    target_model.to(device)
 
 
-
-    x_test, y_test = load_cifar10()
-
-    ids = config['ids']
-    x_test_correct , y_test_correct = x_test[ids,:,:,:] , y_test[ids]
+    x_test, y_test = load_cifar10(n_examples=args.n_examples)
+    x_test, y_test = x_test.to(device), y_test.to(device)
 
     print('Running MI-FGSM ENS attack')
-    attack = taamifgsm_ens.MIFGSM_ENS(source_models[0], source_models, eps=8/255, steps=10, decay=1.0)
-    adv_images_MI_ENS = attack(x_test_correct, y_test_correct)
+    attack = taamifgsm_ens.MIFGSM_ENS(source_models[0], source_models, eps=args.eps, alpha=args.alpha, steps=args.steps, decay=args.decay)
+    adv_images_MI_ENS = attack(x_test, y_test)
 
-    MI_ENS_results = dict()
-    for i,target_model in enumerate(target_models):
-        acc = clean_accuracy(target_model, adv_images_MI_ENS, y_test_correct)
-        print('Robust Acc {}: %2.2f %%'%(acc*100))
-        MI_ENS_results[rank_target_models[i]] = acc
 
-    if not os.path.exists('results'):
-        os.makedirs('results') 
-    
-    file_path = 'results/results_{}_{}.json'.format('_'.join(config['sources']),x_test_correct.size(0))
-
-    if os.path.exists(file_path):
-        with open(file_path,'r') as f:
-            data = json.load(f)
-    else:
-        data = dict()
-
-    data_MIFGSM_ENS = data.get('MI-FGSM ENS',{})
-    data_MIFGSM_ENS.update(MI_ENS_results)
-    data['MI-FGSM ENS'] = data_MIFGSM_ENS
-
-    with open(file_path,'w') as f:
-        json.dump(data, f, indent=4)
+    acc = clean_accuracy(target_model, x_test, y_test) 
+    rob_acc = clean_accuracy(target_model, adv_images_MI_ENS, y_test) 
+    print(args.target, 'Clean Acc: %2.2f %%'%(acc*100))
+    print(args.target, 'Robust Acc: %2.2f %%'%(rob_acc*100))
