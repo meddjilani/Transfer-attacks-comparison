ce8a0cd0de13716098b644c951e6758c67d4cccb
meddjilani <km_djilani@esi.dz>
2023-07-07 18:36:51 +0200

    remove unnecessary files

--- MetaAttack_ICLR2020/meta_attack/meta_attack_cifar/attacks/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cw_black import BlackBoxLInf
-from .generate_gradient import generate_gradient
-
--- MetaAttack_ICLR2020/meta_attack/meta_attack_imagenet/train.sh
+++
@@ -1,1 +0,0 @@
-python train_lossmap.py  --dataset  ped2  --train_folder  ../Data/ped2/training/frames --test_folder  ../Data/ped2/testing/frames --gpu  0 --iters    80000
