1710c0c4f83d8afcbb838296e8709b656ce84011
meddjilani <km_djilani@esi.dz>
2023-09-30 01:51:51 +0100

    add ebad

--- run.sh
+++ run.sh
@@ -1,24 +1,35 @@
-
 #****************************** BASES ****************************** 
-python sm_baseline.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --lr 0.5 --iterw 10 --target resnet18 --surrogate_names resnet50 resnet34
+python attack_BASES.py --untargeted --n_im 100 --loss_name cw --iters 100 --fuse loss --x 3 --iterw 10 --target Standard --surrogate_names Ding2020MMA Zhang2019You Addepalli2022Efficient_WRN_34_10 Sitawarin2020Improving Carmon2019Unlabeled Wu2020Adversarial_extra Huang2021Exploring Xu2023Exploring_WRN-28-10
 
 
-#****************************** OURS ****************************** 
+#****************************** EBAD ****************************** 
+python attack_EBAD.py --untargeted --n_im 100 --loss_name cw --iters 100 --fuse loss --x 3 --iterw 10 --target Standard --surrogate_names Ding2020MMA Zhang2019You Addepalli2022Efficient_WRN_34_10 Sitawarin2020Improving Carmon2019Unlabeled Wu2020Adversarial_extra Huang2021Exploring Xu2023Exploring_WRN-28-10
+
+
+#****************************** SES (ours) ****************************** 
+python attack_SES.py --untargeted --n_im 100 --loss_name cw --iters 100 --fuse loss --x 3 --iterw 10 --target Standard --surrogate_names Ding2020MMA Zhang2019You Addepalli2022Efficient_WRN_34_10 Sitawarin2020Improving Carmon2019Unlabeled Wu2020Adversarial_extra Huang2021Exploring Xu2023Exploring_WRN-28-10
+
+
+#****************************** SES+wb (ours) ****************************** 
+python attack_SES_wb.py --untargeted --n_im 100 --loss_name cw --iters 100 --fuse loss --x 3 --iterw 10 --target Standard --surrogate_names Ding2020MMA Zhang2019You Addepalli2022Efficient_WRN_34_10 Sitawarin2020Improving Carmon2019Unlabeled Wu2020Adversarial_extra Huang2021Exploring Xu2023Exploring_WRN-28-10
+
+
+#****************************** SES (ours) ****************************** 
 #UNTARGETED
-python sm_baseline_ghost.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --lr 0.5 --iterw 10 --target resnet18 --surrogate_names resnet50 resnet34
+python attack_SES.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --iterw 10 --target resnet18 --surrogate_names resnet50 resnet34
 
 
 #TARGETED
-python sm_baseline_ghost.py --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --lr 0.5 --iterw 10 --target resnet18 --surrogate_names resnet50 resnet34
+python attack_SES.py --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --iterw 10 --target resnet18 --surrogate_names resnet50 resnet34
 
 
 #INTER ARCHITECTURE 
-python sm_baseline_ghost.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --lr 0.5 --iterw 10 --target resnet18 --surrogate_names  densenet169 densenet121
+python attack_SES.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --iterw 10 --target resnet18 --surrogate_names  densenet169 densenet121
 
 
 
 #Robust models
-python sm_baseline_ghost.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --lr 0.5 --iterw 10 --target Addepalli2021Towards_RN18 --surrogate_names Sitawarin2020Improving Chen2020Efficient
+python attack_SES.py --untargeted --n_im 10 --loss_name cw --iters 10 --fuse loss --x 3 --iterw 10 --target Addepalli2021Towards_RN18 --surrogate_names Sitawarin2020Improving Chen2020Efficient
 
 
 
