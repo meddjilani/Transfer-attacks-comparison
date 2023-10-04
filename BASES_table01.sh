23eaffc2b831e30161327886ccb08ebc61d32d9b
meddjilani <km_djilani@esi.dz>
2023-10-04 22:59:15 +0100

    BASES table01 untargeted

---
+++ BASES_table01.sh
@@ -0,0 +1,10 @@
+#UNTARGETED
+python attack_SES.py --n_im 1000 --target vgg19 --untargeted
+
+python attack_SES.py --n_im 1000 --target densenet121 --untargeted
+
+python attack_SES.py --n_im 1000 --target resnext50_32x4d --untargeted
+
+
+
+
