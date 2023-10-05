def5adf6473db1a94cd5cdfb001d762de2fdf97d
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-10-05 21:20:56 +0200

    Added success rate per example

--- attack_SES.py
+++ attack_SES.py
@@ -180,8 +180,10 @@ def main():
     query_list_pretend = []
     mean_weights = lambda tab: [sum(col) / len(tab) for col in zip(*tab)] if len(tab) > 0 else []
     w_list = []
+
     for im_idx,(image,label) in tqdm(enumerate(testloader)):
         loss_target = []
+        successful = 0
         print(f"im_idx: {im_idx + 1}")
         image, label = image.to(device), label.to(device)
         with torch.no_grad():
@@ -221,7 +223,7 @@ def main():
             query_list.append(n_query)
             if label.item() == pred_vic:
                 suc_adv +=1
-                
+            successful = 1
         else: 
             idx_w = 0         # idx of wb in W
             last_idx = -1    # if no changes after one round, reduce the learning rate
@@ -251,6 +253,7 @@ def main():
                     if label.item() == pred_vic:
                         suc_adv +=1
                     loss_target.append(loss_plus)
+                    successful = 1
                     break
 
                 w_np_temp_minus = w_np.copy()
@@ -277,6 +280,7 @@ def main():
                     if label.item() == pred_vic:
                         suc_adv +=1
                     loss_target.append(loss_minus)
+                    successful = 1
                     break
 
 
@@ -307,7 +311,9 @@ def main():
         rob_acc = 1-(len(success_idx_list)/(im_idx+1))
         suc_rate = 0 if correct_pred==0 else suc_adv / correct_pred
         w_dict = dict(zip(keys, w_np.tolist()))
-        metrics = {'robust_acc': rob_acc, 'suc_rate' : suc_rate, 'target_correct_pred': correct_pred, 'n_query': n_query, 'loss':loss_target[-1], 'minloss':min(loss_target), 'maxloss':max(loss_target), 'meanloss':sum(loss_target) / len(loss_target)}
+        metrics = {'robust_acc': rob_acc, 'suc_rate':successful,'suc_rate_steps' : suc_rate, 'target_correct_pred': correct_pred, 'n_query': n_query, 'loss':loss_target[-1], 'minloss':min(loss_target), 'maxloss':max(loss_target), 'meanloss':sum(loss_target) / len(loss_target)}
+
+
         metrics.update(w_dict)
         experiment.log_metrics(metrics, step=im_idx+1)
 
