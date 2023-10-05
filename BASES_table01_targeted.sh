c5f9f2f8548efcb4e707cba443e84ed0824805fe
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-10-05 20:59:55 +0200

    Added missing untargeted jobs

--- BASES_table01_targeted.sh
+++ BASES_table01_targeted.sh
@@ -1,8 +1,4 @@
 #TARGETED
 python attack_SES.py --n_im 1000 --target vgg19 --gpu 1
-
-
-
-
-
-
+python attack_SES.py --n_im 1000 --target densenet121 --gpu 1
+python attack_SES.py --n_im 1000 --target resnext50_32x4d --gpu 1
