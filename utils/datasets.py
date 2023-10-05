f4eb58b50b6f5e8f2562c1f9e11454c2feb0b36f
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-10-05 22:20:59 +0200

    Fixed dataset PIL format

--- utils/datasets.py
+++ utils/datasets.py
@@ -3,23 +3,23 @@ from pathlib import Path
 from collections import defaultdict
 from torch.utils.data import Dataset
 from PIL import Image
-import numpy as np
 
-def load_imagenet_1000(dataset_root = "imagenet1000"):
+def load_imagenet_1000(dataset_path = "imagenet1000"):
     """
     Dataset downloaded form kaggle
     https://www.kaggle.com/datasets/google-brain/nips-2017-adversarial-learning-development-set
     Resized from 299x299 to 224x224
 
     Args:
-        dataset_root (str): root folder of dataset
+        dataset_path (str): root folder of dataset
     Returns:
         img_paths (list of strs): the paths of images
         gt_labels (list of ints): the ground truth label of images
         tgt_labels (list of ints): the target label of images
     """
-    dataset_root = Path(dataset_root)
-    img_paths = list(sorted(dataset_root.glob('*.png')))
+    dataset_root = Path(dataset_path)
+    img_root = Path(dataset_path+"/images")
+    img_paths = list(sorted(img_root.glob('*.png')))
     gt_dict = defaultdict(int)
     tgt_dict = defaultdict(int)
     with open(dataset_root / "images.csv", newline='') as csvfile:
@@ -47,16 +47,22 @@ class ImageNet1000Dataset(Dataset):
         self.img_paths = img_paths
         self.gt_labels = gt_labels
         self.tgt_labels = tgt_labels
+        self.transform = transform
+
+        print(f"loaded imagenet1000 with {len(self.img_paths)} images")
 
     def __len__(self):
         return len(self.img_paths)
 
     def __getitem__(self, im_idx):
 
-        im = Image.open(self.img_paths[im_idx]).convert('RGB')
-        image = np.array(im).astype(np.float32) / 255
+        print("loading imagenet1000 of index", im_idx)
+        image = Image.open(self.img_paths[im_idx]).convert('RGB')
 
+
+        gt_label = self.gt_labels[im_idx]
+        tgt_label = self.tgt_labels[im_idx]
         if self.transform:
             image = self.transform(image)
 
-        return (image,self.gt_labels[im_idx],self.tgt_labels[im_idx])
+        return (image,gt_label,tgt_label)
