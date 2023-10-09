cb94668e53ddd24d25c29cb9346f4f288cf82835
meddjilani <km_djilani@esi.dz>
2023-10-09 03:02:04 +0100

    SES ver 2

---
+++ SES_ver2_table01_targeted.sh
@@ -0,0 +1,7 @@
+#TARGETED
+python attack_SES_ver2.py --n_wb 20 --iterw 50 --apply_softmax --reduce_lr --lr 0.01
+python attack_SES_ver2.py --n_wb 20 --iterw 50 --apply_softmax --reduce_lr --lr 0.01 --victim densenet121
+python attack_SES_ver2.py --n_wb 20 --iterw 50 --apply_softmax --reduce_lr --lr 0.01 --victim resnext50_32x4d
+
+
+
