074e101fe4ec873704341e37fd7a9b074807816d
meddjilani <km_djilani@esi.dz>
2023-10-11 13:19:24 +0100

    --random range argument added

--- attack_SES_ver2.py
+++ attack_SES_ver2.py
@@ -32,6 +32,7 @@ def main():
     parser = argparse.ArgumentParser(description="BASES attack")
     parser.add_argument("--victim", nargs="?", default='vgg19', help="victim model")
     parser.add_argument('--ghost', nargs='+', default=['resnet','resnext'])
+    parser.add_argument('--random_range', type=float, default=0.22, help='random range of the uniform distribution perturbation in ghost networks')
     parser.add_argument("--n_wb", type=int, default=10, help="number of models in the ensemble: 4,10,20")
     parser.add_argument("--bound", default='linf', choices=['linf','l2'], help="bound in linf or l2 norm ball")
     parser.add_argument("--eps", type=int, default=16, help="perturbation bound: 10 for linf, 3128 for l2")
